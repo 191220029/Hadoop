19a37513b158d0fe1746db5b24175528d1ccf06f
NJUCS-CC <191220005@smail.nju.edu.cn>
2022-04-15 23:04:49 -0700

    separator

--- lab2/src/main/java/TFIDF.java
+++ lab2/src/main/java/TFIDF.java
@@ -3,13 +3,12 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 import java.io.IOException;
@@ -79,8 +78,8 @@ public class TFIDF {
                     hashMap.put(fileName, hashMap.getOrDefault(fileName, 0) + frequency);
                 }
                 for(String fileName : hashMap.keySet()) {
-                    String TFIDF = decimalFormat.format(IDF * hashMap.get(fileName));
-                    k.set(fileName + "," + prev + "," + TFIDF);
+                    k.set(fileName + "," + prev);
+                    v.set(decimalFormat.format(IDF * hashMap.get(fileName)));
                     context.write(k, v);
                 }
                 stringBuilder.delete(0, stringBuilder.length());
@@ -108,8 +107,8 @@ public class TFIDF {
                 hashMap.put(fileName, hashMap.getOrDefault(fileName, 0) + frequency);
             }
             for(String fileName : hashMap.keySet()) {
-                String TFIDF = decimalFormat.format(IDF * hashMap.get(fileName));
-                k.set(fileName + "," + prev + "," + TFIDF);
+                k.set(fileName + "," + prev);
+                v.set(decimalFormat.format(IDF * hashMap.get(fileName)));
                 context.write(k, v);
             }
         }
@@ -121,6 +120,8 @@ public class TFIDF {
             FileSystem fileSystem = FileSystem.get(conf);
             Integer srcFileNum = fileSystem.listStatus(new Path(args[0])).length;
             conf.set("srcfileCounts", srcFileNum.toString());
+            conf.set("mapred.textoutputformat.ignoreseparator", "true");
+            conf.set("mapred.textoutputformat.separator", ",");
             Job job = new Job(conf, "TF IDF");
             job.setJarByClass(TFIDF.class);
             job.setInputFormatClass(TextInputFormat.class);
@@ -131,6 +132,7 @@ public class TFIDF {
             job.setMapOutputValueClass(IntWritable.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(Text.class);
+            job.setOutputFormatClass(TextOutputFormat.class);
             FileInputFormat.addInputPath(job, new Path(args[0]));
             FileOutputFormat.setOutputPath(job, new Path(args[1]));
             System.exit(job.waitForCompletion(true) ? 0 : 1);
